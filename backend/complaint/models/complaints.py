31a868747e8caf68b6279ead87b55dba36feca87
govinda <sapgovinda05@gmail.com>
2026-05-29 23:14:10 +0545

    feat:working on complaints model

--- backend/complaint/models/complaints.py
+++ backend/complaint/models/complaints.py
@@ -1,13 +1,33 @@
 from django.db import models
 from config.models import BaseModel
 from authx.models import AetherixUsers
+from complaint.models import ComplaintCategory
 
 class ComplaintCategory(BaseModel):
+
+    STATUS_CHOICES = (
+        ("pending", "Pending"),
+        ("reviewing","Reviewing"),
+        ("in_progress","In Progress"),
+        ("resolved","Resolved"),
+        ("rejected","Rejected")
+    )
+    PRIORITY_CHOICES = (
+        ("low", "Low"),
+        ("medium","Medium"),
+        ("high", "High"),
+        ("urgent", "Urgent")
+    )
     user = models.ForeignKKey(
         'authx.AetherixUsers',
         on_delete = models.Protect,
         related_name = "complaints"
 
+    )
+    category = models.ForeignKey(
+        'complaint.ComplaintCategory',
+        on_delete=models.PROTECT,
+        related_name="complaints",
     )
 
     title = models.CharField(
@@ -31,6 +51,20 @@ class ComplaintCategory(BaseModel):
 
     description = models.TextField()
     is_verified = models.BooleanField(default=False)
+    image = models.ImageField(
+        upload_to="complaints/",
+        blank=True,
+        null=True
+    )
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES
+    )
+    priority = models.CharField(
+        max_length=20,
+        choices=PRIORITY_CHOICES,
+        default="medium"
+    )
 
     class Meta:
         db_table = "aetherix_complaints"
