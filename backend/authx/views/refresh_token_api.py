8d527ebd3b37ee99526285e84d957e01ec08807e
govinda <sapgovinda05@gmail.com>
2026-07-06 23:43:13 +0545

    feat:working on refresh token error

--- backend/authx/views/refresh_token_api.py
+++ backend/authx/views/refresh_token_api.py
@@ -9,6 +9,7 @@ class RefreshTokenApiView(BaseApiView):
 
     def post(self, request):
         refresh_token = request.COOKIES.get('refresh_token')
+        print(request.COOKIES)
         if not refresh_token:
             return self.error(
                 message="Refresh token missing.Please Try again.",
