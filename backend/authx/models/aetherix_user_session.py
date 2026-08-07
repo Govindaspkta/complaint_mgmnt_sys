f6e29ceb96f52035c722061da072e65c355a93c1
govinda <sapgovinda05@gmail.com>
2026-08-07 09:07:26 +0545

    fix:updated model with new fields

--- backend/authx/models/aetherix_user_session.py
+++ backend/authx/models/aetherix_user_session.py
@@ -7,3 +7,5 @@ class UserSession(models.Model):
     refresh_token = models.TextField()
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    ip_address = models.GenericIPAddressField(null=True, blank=True)
+    user_agent = models.TextField(null=True,blank=True)
