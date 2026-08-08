7d09973d39024e51fe24f38e38945fa0d9fd74a7
govinda <sapgovinda05@gmail.com>
2026-08-08 23:41:35 +0545

    feat:created new model for complaint department

--- backend/complaint/models/__init__.py
+++ backend/complaint/models/__init__.py
@@ -1,3 +1,4 @@
 from .complaint_categroy import ComplaintCategory 
 from .complaints import AetherixComplaints, ComplaintStatusChoices, ComplaintPriorityChoices
 from .complaint_upvote import *
+from .complaint_department import *
---
+++ backend/complaint/models/complaint_department.py
@@ -0,0 +1,15 @@
+from django.db import models
+from config.models import BaseModel
+
+
+
+class Department(BaseModel):
+    name = models.CharField(max_length=100, unique=True)   # Electricity, Water...
+    email = models.EmailField()
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = "departments"
+
+    def __str__(self):
+        return self.name
