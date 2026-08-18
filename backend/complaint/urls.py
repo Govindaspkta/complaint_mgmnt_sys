b919aca06f01800e8c0a416b20d926466f56568c
govinda <sapgovinda05@gmail.com>
2026-08-18 22:59:08 +0545

    feat:updated url for department

--- backend/complaint/urls.py
+++ backend/complaint/urls.py
@@ -14,4 +14,12 @@ urlpatterns  = [
     path("admin/<reference_id>/", views.ComplaintsAdminUpdateApiView.as_view(), name ="category-by-admin"),
     path("<reference_id>/", views.ComplaintsDetailApiView.as_view(), name ="complaints-detail"),
     path("<str:reference_id>/upvote/", views.AetherixUpvote.as_view(), name='upvote-complaint'),
+
+    path("departments/", views.DepartmentListCreateApiView.as_view(), name="complaint-clusters"),
+    path("departments/<str:reference_id>", views.DepartmentDetailApiView.as_view(), name="complaint-clusters"),
+
+
+
+
 ]
+
