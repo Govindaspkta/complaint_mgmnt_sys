6064a88946d67320e479ce50e5e257ce2bf2926f
govinda <sapgovinda05@gmail.com>
2026-08-24 23:14:35 +0545

    feat:updated

--- backend/complaint/serializers/category_serializer.py
+++ backend/complaint/serializers/category_serializer.py
@@ -34,3 +34,39 @@ class CategorySerializer(serializers.Serializer):
             setattr(instance, field, value)
         instance.save()
         return instance
+
+
+
+class CategorySerializer(serializers.Serializer):
+    reference_id = serializers.CharField(
+        read_only=True
+    )
+    name = serializers.CharField(
+        required=True,
+        error_messages={
+            "required":"Category Name is required."
+        }
+    )
+    display_name = serializers.CharField(
+    required=True,
+        error_messages={
+            "required":"Category Name is required."
+        }
+    )
+    department = serializers.SlugRelatedField(
+        slug_field="reference_id",
+        queryset=Department.objects.filter(is_active=True, is_deleted=False),
+        required=False,
+        allow_null=True
+    )
+
+    description = serializers.CharField()
+
+    def create(self, validated_data):
+        return ComplaintCategory.objects.create(**validated_data)
+
+    def update(self,instance, validated_data):
+        for field, value in validated_data.items():
+            setattr(instance, field, value)
+        instance.save()
+        return instance
