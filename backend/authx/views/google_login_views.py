a635b8d100f5cd3605b141ddf7ff7b25257399cd
govinda <sapgovinda05@gmail.com>
2026-08-27 23:48:10 +0545

    feat:updated with proper messge in oauth along with exception handeling

--- backend/authx/views/google_login_views.py
+++ backend/authx/views/google_login_views.py
@@ -20,62 +20,70 @@ GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
 class GoogleLoginAPIView(PublicApiView):
     
     def post(self, request):
-        serializer = GoogleLoginSerializer(data=request.data)
-        if not serializer.is_valid():
-            return self.error(
-                message="Validation Error.",
-                errors=serializer.errors,
-                status_code=status.HTTP_400_BAD_REQUEST
-            )
-        token = serializer.validated_data.get("token")
         try:
-            idinfo = id_token.verify_oauth2_token(
-                token,
-                requests.Request(),
-                GOOGLE_CLIENT_ID
-            )
-        except ValueError:
-            return self.error( 
-                message="Invalid Google Token",
-                status_code=status.HTTP_400_BAD_REQUEST
-            )
-        email = idinfo.get("email")
-        name = idinfo.get("name")
-
-        user, created = AetherixUsers.objects.get_or_create(
-            email=email,
-            defaults={
-                "email":email,
-                "username":name,
-                }
-        )
-        refresh = RefreshToken.for_user(user)
-        UserSession.objects.create(
-                    user=user,
-                    refresh_token=str(refresh),
-                    ip_address =request.META.get('REMOTE_ADDR'),
-                    user_agent=request.META.get('HTTP_USER_AGENT')
+            serializer = GoogleLoginSerializer(data=request.data)
+            if not serializer.is_valid():
+                return self.error(
+                    message="Validation Error.",
+                    errors=serializer.errors,
+                    status_code=status.HTTP_400_BAD_REQUEST
                 )
-        response_data = { 
-            "access":str(refresh.access_token),
-            "refresh": str(refresh),
-            "user":{
-                "id":user.id,
-                "email":user.email,
-                "username":user.username
+            token = serializer.validated_data.get("token")
+            try:
+                idinfo = id_token.verify_oauth2_token(
+                    token,
+                    requests.Request(),
+                    GOOGLE_CLIENT_ID
+                )
+            except ValueError:
+                return self.error( 
+                    message="Invalid Google Token",
+                    status_code=status.HTTP_400_BAD_REQUEST
+                )
+            email = idinfo.get("email")
+            name = idinfo.get("name")
+
+            user, created = AetherixUsers.objects.get_or_create(
+                email=email,
+                defaults={
+                    "email":email,
+                    "username":name,
+                    }
+            )
+            refresh = RefreshToken.for_user(user)
+            UserSession.objects.create(
+                        user=user,
+                        refresh_token=str(refresh),
+                        ip_address =request.META.get('REMOTE_ADDR'),
+                        user_agent=request.META.get('HTTP_USER_AGENT')
+                    )
+            response_data = { 
+                "access":str(refresh.access_token),
+                "refresh": str(refresh),
+                "user":{
+                    "id":user.id,
+                    "email":user.email,
+                    "username":user.username
+                }
             }
-        }
-        response = self.success(
-            data=response_data,
-            message ="Google Login Successful",
-            status_code =status.HTTP_200_OK
-        )
-        response.set_cookie(
-            key="refresh_token",
-            value=str(refresh),
-            httponly=True,
-            secure=False,
-            samesite="Lax",
-            max_age=24*60*60
-        )
-        return response
+            response = self.success(
+                data=response_data,
+                message ="Google Login Successful",
+                status_code =status.HTTP_200_OK
+            )
+            response.set_cookie(
+                key="refresh_token",
+                value=str(refresh),
+                httponly=True,
+                secure=False,
+                samesite="Lax",
+                max_age=24*60*60
+            )
+            return response
+        except Exception as e:
+            logger.exception("Google login failed.", str(e))
+
+            return self.error(
+                  message="Unable to process your login at the moment. Please try again later.",
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
