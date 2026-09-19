594df5433209d72fdf764167d99816e4a84dfcde
govinda <sapgovinda05@gmail.com>
2026-09-19 13:48:27 +0545

    fea:updated caching for production

--- backend/complaint_sys/settings/production.py
+++ backend/complaint_sys/settings/production.py
@@ -31,7 +31,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': config('REDIS_URL'),
+        'LOCATION': config('REDIS_URL', default='redis://127.0.0.1:6379/1'),
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         },
